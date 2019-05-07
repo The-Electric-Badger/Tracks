320067f8fa0884ae8354f6cd36c9c227d92f8d53
Paul Sinnett <paul.sinnett@gmail.com>
2019-05-07 20:11:58 +0100

    Initial commit

---
+++ Assets/Track.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+	int distance = 3;
+	public Track previous;
+	new public Renderer light;
+	Collider train = null;
+
+	void SetDistance(int toTrain)
+	{
+		distance = toTrain;
+
+		if (previous != null && train == null)
+		{
+			previous.SetDistance(toTrain + 1);
+		}
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+		train = collider;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+		train = null;
+		SetDistance(0);
+    }
+
+	void Update()
+	{
+		switch (distance)
+		{
+			case 0:
+			case 1:
+				light.material.color = Color.red;
+				break;
+			case 2:
+				light.material.color = Color.yellow;
+				break;
+			default:
+				light.material.color = Color.green;
+				break;
+		}
+	}
+}
